746ecbc50ac7d7a42977c4e30f418a80d650bcba
Matt Fielding <matt@hippodigital.co.uk>
2017-11-06 11:40:14 +0000

    creating historial versions of offline vouching service

--- app/routes/vouching-3.js
+++ app/routes/vouching-3.js
@@ -1,9 +1,18 @@
 module.exports = function (router) {
-  router.get('/clinical/vouching-3', function (req, res) {
+  router.get('/vouching/vouching-3', function (req, res) {
     // pull in the url parameter
     var theReason = req.param('reason')
     // re-render the page along with the parameter
-    res.render('clinical/vouching-3', {reason: theReason}, function(err, html) {
+    res.render('vouching/vouching-3', {reason: theReason}, function(err, html) {
+      res.send(html)
+    })
+  })
+
+  router.get('/vouching/v2/vouching-3', function (req, res) {
+    // pull in the url parameter
+    var theReason = req.param('reason')
+    // re-render the page along with the parameter
+    res.render('vouching/v2/vouching-3', {reason: theReason}, function(err, html) {
       res.send(html)
     })
   })
