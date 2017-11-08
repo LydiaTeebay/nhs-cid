2e44e64b39930238c5f80f946f1e923ef41a05b5
Matt Fielding <matt@hippodigital.co.uk>
2017-11-08 23:51:11 +0000

    prove-your-identity improvements

--- assets/scripts/app/app.js
+++ assets/scripts/app/app.js
@@ -5,53 +5,36 @@ function readURL(input, idType) {
     var reader = new FileReader()
 
     reader.onload = function (e) {
-
-      if (idType == "photo") {
-        $("#scan-photo-preview").css("display", "block")
-        $('#uploaded-photo').attr('src', e.target.result)
-        $('#photo-review').attr('src', e.target.result)
-
-      }
       if (idType == "id") {
-        $("#scan-id-preview").css("display", "block")
+        $("#scan-id-1").css("display", "none")
+        $("#scan-id-2").css("display", "block")
         $('#uploaded-id').attr('src', e.target.result)
-        $('#id-review').attr('src', e.target.result)
-
+      }
+      if (idType == "video") {
+        $("#scan-id-3").css("display", "none")
+        $("#scan-id-4").css("display", "block")
+        $('#uploaded-video>source').attr('src', e.target.result)
       }
     }
-    reader.readAsDataURL(input.files[0]);
+    reader.readAsDataURL(input.files[0])
   }
 }
 
-$("#photo-document").change(function(){
-  readURL(this, "photo")
-  $("#scan-photo-button").removeAttr("disabled")
-});
-
 $("#id-document").change(function(){
   readURL(this, "id")
-  $("#scan-id-button").removeAttr("disabled")
-});
+})
 
-$("#scan-id-button").on("click", function(e) {
+// submit photo button action
+$("#submit-id-button").on("click", function(e) {
   e.preventDefault()
-  if ($("#scan-id-button").attr("disabled") == "disabled") {
-
-  }
-  $("#scan-id-1").css("display","none")
-  $("#scan-id-2").css("display","block")
-  document.body.scrollTop = document.documentElement.scrollTop = 0
-});
-
-$("#scan-photo-button").on("click", function(e) {
-  e.preventDefault()
-  if ($("#scan-photo-button").attr("disabled") == "disabled") {
-
-  }
   $("#scan-id-2").css("display","none")
   $("#scan-id-3").css("display","block")
   document.body.scrollTop = document.documentElement.scrollTop = 0
-});
+})
+
+$("#id-selfie").change(function(){
+  readURL(this, "video")
+})
 
 $(document).ready(function () {
   // Turn off jQuery animation
