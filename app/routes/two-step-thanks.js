24941966c58fa69c241f781d06e6bedeaa4b960d
Matt Fielding <matt@hippodigital.co.uk>
2017-11-13 10:26:47 +0000

    another routing fix

--- app/routes/two-step-thanks.js
+++ app/routes/two-step-thanks.js
@@ -8,7 +8,7 @@ module.exports = function (router) {
       return
     }
     // re-render the page along with the parameter
-    res.render('/create-account/two-step-thanks', {vouch: vouched}, function(err, html) {
+    res.render('create-account/two-step-thanks', {vouch: vouched}, function(err, html) {
       res.send(html)
     })
   })
