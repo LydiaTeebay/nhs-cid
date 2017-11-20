47a286b26f9666ed200bc7ea12ea59fecab85cff
Matt Fielding <matt@hippodigital.co.uk>
2017-11-20 22:39:44 +0000

    added sign in routing

--- app/routes/email-validate.js
+++ app/routes/email-validate.js
@@ -2,11 +2,12 @@ module.exports = function (router) {
   router.get('/emails/email-validate', function (req, res) {
     // pull in the url parameters
     var theReason = req.param('reason')
+    var passwordChange = req.param('passwordChange')
     var vouched = req.param('vouch')
     var service = req.param('service')
     var serviceName = req.param('serviceName')
     // re-render the page along with the parameter
-    res.render('emails/email-validate', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
+    res.render('emails/email-validate', {reason: theReason, passwordChange: passwordChange, vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
       res.send(html)
     })
   })
