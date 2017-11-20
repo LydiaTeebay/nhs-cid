c1369fdba9ceb33904304bc21eb54941dcbc4807
Matt Fielding <matt@hippodigital.co.uk>
2017-11-20 17:19:48 +0000

    routing updates

--- app/routes/create-acc.js
+++ app/routes/create-acc.js
@@ -4,6 +4,10 @@ module.exports = function (router) {
     var vouched = req.param('vouch')
     var service = req.param('service')
     var serviceName = req.param('serviceName')
+    if (vouched === 'yes') {
+          res.redirect('/create-account/two-step-code?vouch=yes')
+          return
+      }
     // re-render the page along with the parameter
     res.render('create-account/create-acc', {vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
       res.send(html)
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,6 +2,7 @@ module.exports = function (router) {
   router.get('/account/dashboard', function (req, res) {
     // pull in the url parameter
     var vouched = req.param('vouch')
+    var vouchNew = req.param('vouchNew')
     var later = req.param('later')
     var service = req.param('service')
     var serviceName = req.param('serviceName')
@@ -12,7 +13,7 @@ module.exports = function (router) {
     }
 
     // re-render the page along with the parameter
-    res.render('account/dashboard', {vouch: vouched, service: service, serviceName: serviceName, later: later}, function(err, html) {
+    res.render('account/dashboard', {vouch: vouched, vouchNew: vouchNew, service: service, serviceName: serviceName, later: later}, function(err, html) {
       res.send(html)
     })
   })
