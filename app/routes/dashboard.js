19ac72677458d454932a93203df6131e0862f060
Matt Fielding <matt@hippodigital.co.uk>
2017-11-20 15:58:21 +0000

    pyi success page changes

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,6 +2,7 @@ module.exports = function (router) {
   router.get('/account/dashboard', function (req, res) {
     // pull in the url parameter
     var vouched = req.param('vouch')
+    var later = req.param('later')
     var service = req.param('service')
     var serviceName = req.param('serviceName')
     // redirect to uplift page
@@ -11,7 +12,7 @@ module.exports = function (router) {
     }
 
     // re-render the page along with the parameter
-    res.render('account/dashboard', {vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
+    res.render('account/dashboard', {vouch: vouched, service: service, serviceName: serviceName, later: later}, function(err, html) {
       res.send(html)
     })
   })
