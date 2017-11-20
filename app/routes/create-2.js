28b7f0a231fe67b0ae292093706ef2daba436ecf
Matt Fielding <matt@hippodigital.co.uk>
2017-11-20 08:33:56 +0000

    clean out unused files

--- app/routes/create-2.js
+++ app/routes/create-2.js
@@ -1,9 +1,9 @@
 module.exports = function (router) {
-  router.get('/create-account/create-2', function (req, res) {
+  router.get('/create-account/v1/create-2', function (req, res) {
     // pull in the url parameter
     var vouched = req.param('vouch')
     // re-render the page along with the parameter
-    res.render('create-account/create-2', {vouch: vouched}, function(err, html) {
+    res.render('create-account/v1/create-2', {vouch: vouched}, function(err, html) {
       res.send(html)
     })
   })
