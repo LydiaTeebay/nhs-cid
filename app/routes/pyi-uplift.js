fdedb081eecef2e21751dfbc338423ac5012cea5
Matt Fielding <matt@hippodigital.co.uk>
2017-11-21 17:00:25 +0000

    pyi image upload improvements

--- app/routes/pyi-uplift.js
+++ app/routes/pyi-uplift.js
@@ -10,6 +10,17 @@ module.exports = function (router) {
       res.send(html)
     })
   })
+    router.get('/prove-your-identity/v2/pyi-uplift', function (req, res) {
+        // pull in the url parameters
+        var theReason = req.param('reason')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        // re-render the page along with the parameter
+        res.render('prove-your-identity/v2/pyi-uplift', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
+            res.send(html)
+        })
+    })
     router.get('/prove-your-identity/v1/pyi-uplift', function (req, res) {
         // pull in the url parameters
         var theReason = req.param('reason')
