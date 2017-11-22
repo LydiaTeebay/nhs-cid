4ca5778e517837f719d9fab1a34922fa82c4cf05
Matt Fielding <matt@hippodigital.co.uk>
2017-11-22 12:21:37 +0000

    routing fixes to older versions

--- app/routes/patient-access-2.js
+++ app/routes/patient-access-2.js
@@ -9,4 +9,14 @@ module.exports = function (router) {
       res.send(html)
     })
   })
+    router.get('/patient-online/v3/patient-access-2', function (req, res) {
+        // pull in the url parameter
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        // re-render the page along with the parameter
+        res.render('patient-online/v3/patient-access-2', {vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
+            res.send(html)
+        })
+    })
 }
