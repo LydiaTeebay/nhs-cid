d2db02106e2feac6b86b6bcb0f8869fd42f44b64
Matt Fielding <matt@hippodigital.co.uk>
2017-12-01 15:23:32 +0000

    add notify integration

--- app/routes/check-email.js
+++ app/routes/check-email.js
@@ -1,3 +1,8 @@
+// notify integration
+let apiKey = 'cidprototype-96856a46-bebf-4032-881a-d7b35262e5c8-8433d391-d07a-484d-86b0-0406a2203a01'
+let templateId = 'b54b3862-3aef-4237-9b5d-358f43ec95c0'
+const NotifyClient = require('notifications-node-client').NotifyClient, notifyClient = new NotifyClient(apiKey)
+
 module.exports = function (router) {
   router.get('/create-account/check-email', function (req, res) {
     // pull in the url parameters
@@ -8,6 +13,21 @@ module.exports = function (router) {
     var terms = req.param('terms')
     var emailAddress = req.param('emailAddress')
     var mobileNum = req.param('mobileNum')
+
+    // send email message
+    var params = '?service=' + service + '&serviceName=' + '&vouch=' + vouched + '&terms=' + terms + '&emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&reason=' + theReason
+    var personalisation = {
+        'email_address': emailAddress,
+        'params': params
+    }
+    if (emailAddress !== '' || emailAddress !== 'undefined') {
+        notifyClient
+            .sendEmail(templateId, emailAddress, {
+                personalisation: personalisation
+            })
+            .then(response => console.log(response)
+            ).catch(err => console.error(err))
+    }
     // re-render the page along with the parameter
     res.render('create-account/check-email', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName, terms: terms, emailAddress: emailAddress, mobileNum: mobileNum }, function(err, html) {
       res.send(html)
