fb8f6c92df1685b65baa2b1180aac63ae20b1b50
Matt Fielding <matt@hippodigital.co.uk>
2017-12-11 15:10:58 +0000

    Create new snapshot of the ceate account pages

--- app/routes/login-nhs.js
+++ app/routes/login-nhs.js
@@ -20,4 +20,25 @@ module.exports = function (router) {
       })
     }
   })
+    router.get('/create-account/v3/login-nhs', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var newUser = req.param('newUser')
+        var serviceName = req.param('serviceName')
+        var passwordChange = req.param('passwordChange')
+        var terms = req.param('terms')
+        var emailAddress = req.param('emailAddress')
+        var mobileNum = req.param('mobileNum')
+        // re-render the page along with the parameter
+
+        if (terms === 'yes') {
+            res.redirect('/create-account/v3/two-step-code-login?service=' + service + '&serviceName=' + serviceName + '&terms=' + terms + '&mobileNum=' + mobileNum + '&emailAddress=' + emailAddress + '&vouch=' + vouched)
+            return
+        } else {
+            res.render('create-account/v3/login-nhs', {vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms }, function(err, html) {
+                res.send(html)
+            })
+        }
+    })
 }
--- app/routes/two-step-code-login.js
+++ app/routes/two-step-code-login.js
@@ -33,4 +33,33 @@ module.exports = function (router) {
       res.send(html)
     })
   })
+
+    router.get('/create-account/v3/two-step-code-login', function (req, res) {
+        // pull in the url parameters
+        var theReason = req.param('reason')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var terms = req.param('terms')
+        var resend = req.param('resend')
+        var mobileNum = req.param('mobileNum')
+        var pinCode = Math.floor(1000 + Math.random() * 9000)
+        var personalisation = {
+            'pincode': pinCode
+        }
+        // send text message
+
+        if (mobileNum !== '' || mobileNum !== 'undefined') {
+            notifyClient
+                .sendSms(templateId, mobileNum, {
+                    personalisation: personalisation,
+                    smsSenderId: smsSender })
+                .then(response => console.log(response))
+        .catch(err => console.error(err))
+        }
+        // re-render the page along with the parameter
+        res.render('create-account/v3/two-step-code-login', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName, terms: terms, resend: resend, mobileNum: mobileNum }, function(err, html) {
+            res.send(html)
+        })
+    })
 }
