f0bfbae4aa44b3cf1d7979347e77acfcfbf0f32d
Matt Fielding <matt@hippodigital.co.uk>
2017-12-12 15:39:30 +0000

    add css and js

---
+++ assets/scripts/app/onfido.js
@@ -0,0 +1,35 @@
+var Onfido = require('onfido-sdk-ui');
+
+window.onload = function() {
+    var url = location.protocol+"//"+location.hostname+":8090/jwt"
+    var request = new XMLHttpRequest()
+    request.open('GET', url, true)
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var data = JSON.parse(request.responseText)
+            window.onfidoOut = Onfido.init({
+                useModal: false,
+                token: data.token,
+                onComplete: function() {
+                    // callback for when everything is complete
+                    console.log("Everything is complete");
+                },
+                steps: [
+                    {
+                        type:'welcome',
+                        options:{
+                            title:'Open your new bank account'
+                        }
+                    },
+                    'document',
+                    'face',
+                    'complete'
+                ]
+            });
+        }
+    }
+    request.send()
+};
+
+
+require("./app.css")
