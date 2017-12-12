50bd32764355fbdc09c1e3b30656c1cbb675f0bb
Matt Fielding <matt@hippodigital.co.uk>
2017-12-12 15:06:20 +0000

    snapshot of pyi journey

--- app/routes/pyi-continue-offline-map.js
+++ app/routes/pyi-continue-offline-map.js
@@ -8,4 +8,13 @@ module.exports = function (router) {
             res.send(html)
         })
     })
+    router.get('/prove-your-identity/v4/pyi-continue-offline-map', function (req, res) {
+        // pull in the url parameter
+        var theReason = req.param('reason')
+        var isMobile = req.useragent.isMobile
+        // re-render the page along with the parameter
+        res.render('prove-your-identity/v4/pyi-continue-offline-map', {reason: theReason, mobile: isMobile}, function(err, html) {
+            res.send(html)
+        })
+    })
 }
--- app/routes/pyi-uplift-2.js
+++ app/routes/pyi-uplift-2.js
@@ -11,4 +11,16 @@ module.exports = function (router) {
       res.send(html)
     })
   })
+    router.get('/prove-your-identity/v4/pyi-uplift-2', function (req, res) {
+        // pull in the url parameters
+        var theReason = req.param('reason')
+        var vouched = req.param('vouch')
+        var verify = req.param('verify')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        // re-render the page along with the parameter
+        res.render('prove-your-identity/v4/pyi-uplift-2', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName, verify: verify}, function(err, html) {
+            res.send(html)
+        })
+    })
 }
