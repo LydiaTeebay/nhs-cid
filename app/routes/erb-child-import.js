23b7b1d6ce50da0e6234e09cd17a4f7506ee496d
Matt Fielding <matt@hippodigital.co.uk>
2018-01-09 15:14:29 +0000

    parameter setting updates

--- app/routes/erb-child-import.js
+++ app/routes/erb-child-import.js
@@ -1,13 +1,13 @@
 module.exports = function (router) {
-    router.get('/service-access/service-access-eredbook-02', function (req, res) {
+    router.get('/eRedbook/erb-child-import', function (req, res) {
         // pull in the url parameters
-        var vouched = req.param('vouch')
+        console.log("HHDSDSAHDAS")
         var service = req.param('service')
         var serviceName = req.param('serviceName')
         var mobileNum = req.param('mobileNum')
         var emailAddress = req.param('emailAddress')
         // re-render the page along with the parameter
-        res.render('service-access/service-access-eredbook-02', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum }, function(err, html) {
+        res.render('eRedbook/erb-child-import', { service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum }, function(err, html) {
             res.send(html)
         })
     })
