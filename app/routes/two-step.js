c95cbcca764f80d7856867a1c2f68b720e8a6612
Matt Fielding <matt@hippodigital.co.uk>
2018-01-16 12:32:08 +0000

    routing fixes

--- app/routes/two-step.js
+++ app/routes/two-step.js
@@ -43,4 +43,15 @@ module.exports = function (router) {
             res.send(html)
         })
     })
+    router.get('/create-account/v2/two-step', function (req, res) {
+        // pull in the url parameters
+        var theReason = req.param('reason')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        // re-render the page along with the parameter
+        res.render('create-account/v2/two-step', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName}, function(err, html) {
+            res.send(html)
+        })
+    })
 }
