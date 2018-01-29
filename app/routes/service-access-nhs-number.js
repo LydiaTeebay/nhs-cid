31109c1f5155a5a9b2c4196213417a434ef8bc20
Matt Fielding <matt@hippodigital.co.uk>
2018-01-29 23:34:10 +0000

    new service access pages and archived old ones

---
+++ app/routes/service-access-nhs-number.js
@@ -0,0 +1,15 @@
+module.exports = function (router) {
+    router.get('/service-access/service-access-nhs-number', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        // re-render the page along with the parameter
+        res.render('service-access/service-access-nhs-number', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror }, function(err, html) {
+            res.send(html)
+        })
+    })
+}
