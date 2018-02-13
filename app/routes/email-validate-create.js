d1519a4c4a6587ecf91f5eb73702e4e98c62eaab
Chris Ball <ChrisBall@Christophers-MacBook-Air.local>
2018-02-13 10:45:38 +0000

    Flow changes for create account in pol

---
+++ app/routes/email-validate-create.js
@@ -0,0 +1,9 @@
+module.exports = function (router) {
+  router.get('/emails/email-validate-create', function (req, res) {
+    var service = req.param('service')
+    var serviceName = req.param('serviceName')
+    res.render('emails/email-validate-create', { serviceName: serviceName }, function(err, html) {
+      res.send(html)
+    })
+  })
+}
---
+++ app/routes/login-nhs-create.js
@@ -0,0 +1,9 @@
+module.exports = function (router) {
+  router.get('/create-account/login-nhs-create', function (req, res) {
+    var service = req.param('service')
+    var serviceName = req.param('serviceName')
+    res.render('create-account/login-nhs-create', { serviceName: serviceName }, function(err, html) {
+      res.send(html)
+    })
+  })
+}
