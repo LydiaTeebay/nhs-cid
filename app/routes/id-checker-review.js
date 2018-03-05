6c91bc988042e8cb78f08a3f7bdba5de96e334df
Matt Fielding <matt@hippodigital.co.uk>
2018-03-05 16:12:40 +0000

    id checker dashboard design

---
+++ app/routes/id-checker-review.js
@@ -0,0 +1,9 @@
+module.exports = function (router) {
+    router.get('/id-checker/id-checker-review', function (req, res) {
+        // pull in the url parameter
+        var state = req.param('state')
+        res.render('id-checker/id-checker-review', {state: state}, function(err, html) {
+            res.send(html)
+        })
+    })
+}
