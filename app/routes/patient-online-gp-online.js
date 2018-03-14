2d4e239e6c9b2d9c58c3b3a3a3278dd547e1551f
monsterlimo <mattnixon79@googlemal.com>
2018-03-14 08:28:32 +0000

    pol revision

--- app/routes/patient-online-gp-online.js
+++ app/routes/patient-online-gp-online.js
@@ -8,6 +8,15 @@ module.exports = function (router) {
     })
   })
 
+  router.get('/patient-online/v6/patient-online-gp-online', function (req, res) {
+    //var service = req.param('service')
+    var serviceName = req.param('serviceName');
+    var hidehead = req.param('hidehead');
+    res.render('patient-online/v6/patient-online-gp-online', { serviceName: serviceName, hidehead: hidehead }, function(err, html) {
+      res.send(html)
+    })
+  })
+  
   router.get('/patient-online/v5/patient-online-gp-online', function (req, res) {
     //var service = req.param('service')
     var serviceName = req.param('serviceName')
