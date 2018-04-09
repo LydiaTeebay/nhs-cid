2a24b4f928401ec47c15a429c50ceed769e9a4da
Matt Fielding <matt@hippodigital.co.uk>
2018-04-09 10:24:26 +0100

    nhs number input changes

--- app/routes/service-access-nhs-number.js
+++ app/routes/service-access-nhs-number.js
@@ -14,6 +14,24 @@ module.exports = function (router) {
         })
     })
 
+    router.post('/service-access/service-access-nhs-number', function (req, res) {
+        // var nhs_yes = req.body.radioinlinegroup
+        // var nhs_number = req.body.nhs_number
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        var hidehead = req.param('hidehead')
+
+        // if (nhs_yes === 'yes') {
+        //     check('nhs_number').exists()
+        //     check('nhs_number', 'NHS numbers must be at least 10 digits and can contain spaces').isLength({min: 10}).isNumeric()
+        // }
+
+        res.redirect('/service-access/service-access-photo-id?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + "&serviceName=" + serviceName + "&formerror=" + formerror + "&hidehead=" + hidehead )
+    })
+
     router.get('/service-access/v4/service-access-nhs-number', function (req, res) {
         // pull in the url parameters
         var vouched = req.param('vouch')
