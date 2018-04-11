accd1d4342dc6eb3af8aaf3eee6473c8573a1715
Matt Fielding <matt@hippodigital.co.uk>
2018-04-11 09:55:13 +0100

    new flow for NHS number

--- app/routes/service-access-nhs-number.js
+++ app/routes/service-access-nhs-number.js
@@ -15,21 +15,19 @@ module.exports = function (router) {
     })
 
     router.post('/service-access/service-access-nhs-number', function (req, res) {
-        // var nhs_yes = req.body.radioinlinegroup
-        // var nhs_number = req.body.nhs_number
+        var nhs_yes = req.body.radioinlinegroup.val()
         var service = req.param('service')
         var serviceName = req.param('serviceName')
         var mobileNum = req.param('mobileNum')
         var emailAddress = req.param('emailAddress')
         var formerror = req.param('formerror')
         var hidehead = req.param('hidehead')
+        console.log(nhs_yes)
+        if (nhs_yes === 'yes') {
 
-        // if (nhs_yes === 'yes') {
-        //     check('nhs_number').exists()
-        //     check('nhs_number', 'NHS numbers must be at least 10 digits and can contain spaces').isLength({min: 10}).isNumeric()
-        // }
+        }
 
-        res.redirect('/service-access/service-access-photo-id?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + "&serviceName=" + serviceName + "&formerror=" + formerror + "&hidehead=" + hidehead )
+        res.redirect('/service-access/service-access-nhs-number-demographics-name?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + "&serviceName=" + serviceName + "&formerror=" + formerror + "&hidehead=" + hidehead )
     })
 
     router.get('/service-access/v4/service-access-nhs-number', function (req, res) {
