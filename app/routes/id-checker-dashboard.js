ef7c888319e802dd69551dab15d6f296b053b5e7
Matt Fielding <matt@hippodigital.co.uk>
2018-04-23 23:43:26 +0100

    updated routing

--- app/routes/id-checker-dashboard.js
+++ app/routes/id-checker-dashboard.js
@@ -2,7 +2,23 @@ module.exports = function (router) {
     router.get('/id-checker/id-checker-dashboard', function (req, res) {
         // pull in the url parameter
         var state = req.param('state')
-        res.render('id-checker/id-checker-dashboard', {state: state}, function(err, html) {
+        var role = req.param('role')
+        var emailLink = req.param('emailLink')
+
+        if (role === 'Supervisor') {
+            res.redirect('/id-checker/id-checker-supervisor-dashboard?role=Supervisor')
+            return
+        }
+        res.render('id-checker/id-checker-dashboard', {state: state, emailLink: emailLink, role: role}, function(err, html) {
+            res.send(html)
+        })
+
+    })
+    router.get('/id-checker/v2/id-checker-dashboard', function (req, res) {
+        // pull in the url parameter
+        var state = req.param('state')
+        var emailLink = req.param('emailLink')
+        res.render('id-checker/v2/id-checker-dashboard', {state: state, emailLink: emailLink}, function(err, html) {
             res.send(html)
         })
     })
