b1a33c30dd19fd4dee2712fb0e773501f7791f8b
monsterlimo <mattnixon79@googlemal.com>
2018-04-25 10:07:19 +0100

    updated the latest pol user journey

---
+++ app/routes/patient-online-find-letter.js
@@ -0,0 +1,10 @@
+module.exports = function (router) {
+  router.get('/patient-online/patient-online-find-letter', function (req, res) {
+    var service = req.param('service')
+    var serviceName = req.param('serviceName');
+    var hidehead = req.param('hidehead');
+    res.render('patient-online/patient-online-find-letter', { serviceName: serviceName, hidehead: hidehead, service: service }, function(err, html) {
+      res.send(html)
+    })
+  })
+}
