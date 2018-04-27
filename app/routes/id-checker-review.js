5ece8daebc695c7917ede1df09ad10927a8683ba
Matt Fielding <matt@hippodigital.co.uk>
2018-04-27 15:22:17 +0100

    supervisor role routing

--- app/routes/id-checker-review.js
+++ app/routes/id-checker-review.js
@@ -2,7 +2,8 @@ module.exports = function (router) {
     router.get('/id-checker/id-checker-review', function (req, res) {
         // pull in the url parameter
         var state = req.param('state')
-        res.render('id-checker/id-checker-review', {state: state}, function(err, html) {
+        var role = req.param('role')
+        res.render('id-checker/id-checker-review', {state: state, role: role}, function(err, html) {
             res.send(html)
         })
     })
