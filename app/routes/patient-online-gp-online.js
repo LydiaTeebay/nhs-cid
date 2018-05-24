764f21e224c59bb5261055b443484ecdd2cc2c61
Chris Ball <ChrisBall@Christophers-MacBook-Air.local>
2018-05-24 10:58:37 +0100

    creating new path for June 6th research

--- app/routes/patient-online-gp-online.js
+++ app/routes/patient-online-gp-online.js
@@ -8,14 +8,23 @@ module.exports = function (router) {
     })
   })
 
-    router.get('/patient-online/mvp/patient-online-gp-online', function (req, res) {
-        var service = req.param('service')
-        var serviceName = req.param('serviceName');
-        var hidehead = req.param('hidehead');
-        res.render('patient-online/mvp/patient-online-gp-online', { serviceName: serviceName, hidehead: hidehead, service: service }, function(err, html) {
-            res.send(html)
-        })
-    })
+  router.get('/patient-online/v9/patient-online-gp-online', function (req, res) {
+      var service = req.param('service')
+      var serviceName = req.param('serviceName');
+      var hidehead = req.param('hidehead');
+      res.render('patient-online/v9/patient-online-gp-online', { serviceName: serviceName, hidehead: hidehead, service: service }, function(err, html) {
+          res.send(html)
+      })
+  })
+
+  router.get('/patient-online/mvp/patient-online-gp-online', function (req, res) {
+      var service = req.param('service')
+      var serviceName = req.param('serviceName');
+      var hidehead = req.param('hidehead');
+      res.render('patient-online/mvp/patient-online-gp-online', { serviceName: serviceName, hidehead: hidehead, service: service }, function(err, html) {
+          res.send(html)
+      })
+  })
 
   router.get('/patient-online/v7/patient-online-gp-online', function (req, res) {
     var service = req.param('service')
