39c49facd4a59541255bf4c93eadd927366c3a24
Matt Fielding <matt@hippodigital.co.uk>
2018-06-13 12:49:47 +0100

    routing between sections

--- app/routes/patient-online-gp-results.js
+++ app/routes/patient-online-gp-results.js
@@ -15,6 +15,80 @@ module.exports = function (router) {
     })
   })
 
+    router.get('/patient-online/v9/patient-online-gp-results', function (req, res) {
+        var postcode = req.param('postcode')
+        var search = req.param('search')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+
+        let url = "https://beta.nhs.uk/book-a-gp-appointment/results?search=" + search + "&postcode=" + postcode
+
+        var gpResults = ''
+        //console.log("TESTING!!!");
+
+        var options = {
+            uri: url,
+            transform: function (body) {
+                return cheerio.load(body)
+            }
+        };
+
+        rp(options)
+            .then(function ($) {
+                $('.results__address').each(function(index) {
+                })
+
+                $('li > div').each(function(index) {
+                    $(this).find(".results__address:last-child").remove()
+                });
+
+                $('li').each(function(index) {
+                    if (index > 9) {
+                        $(this).addClass('hide-item')
+                    }
+                })
+
+                $('h3').each(function( index ) {
+
+                    if (!$(this).has("a").length) {
+                        var textNode = $(this).text()
+                        var link = $("<a>")
+                        link.attr("href", "patient-online-british-id")
+                        link.attr("title", $(this).text())
+                        link.text($(this).text())
+                        $(this).html(link)
+                    } else {
+
+                        var systemQuery = "patient-online-gp-online?" + "service=" + service + "&serviceName=" + serviceName + "&hidehead=" + hidehead
+                        var systemUrl = $(this).find('a').attr('href')
+                        var surgeryName = $(this).find('a').text()
+
+                        if (systemUrl.indexOf("systmonline") !== -1) {
+                            $(this).find('a').attr('href', systemQuery + "&system=tpp" + "&surgery=" + surgeryName)
+                        }
+
+                        if (systemUrl.indexOf('emisaccess') !== -1) {
+                            $(this).find('a').attr('href', systemQuery + "&system=emis"+ "&surgery=" + surgeryName)
+                        }
+                    }
+                })
+
+                data = $('.grid-row').html()
+                gpResults = data
+
+                res.render('patient-online/v9/patient-online-gp-results', { serviceName: serviceName, service: service, hidehead: hidehead, postcode: postcode, search: search, gpResults: gpResults  }, function(err, html) {
+                    res.send(html)
+                })
+            })
+            .catch(function (err) {
+                // Crawling failed or Cheerio choked...
+            })
+
+        console.log(gpResults)
+
+    })
+
   router.get('/patient-online/v8/patient-online-gp-results', function (req, res) {
     var postcode = req.param('postcode');
     var search = req.param('search');
