61db9d18635e1eea660c15fd116263d240c5fae3
Matt Fielding <matt@hippodigital.co.uk>
2018-07-18 15:59:47 +0100

    first stab at simple naming study steps

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,14 @@ module.exports = function (router) {
       title: 'NHSUK prototype kit'
     })
   })
+
+    router.get('/naming-study/index', function (req, res) {
+        // pull in the url parameter
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('naming-study/index', {lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy}, function(err, html) {
+            res.send(html)
+        })
+    })
 }
