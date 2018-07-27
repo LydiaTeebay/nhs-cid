62f49c76847224b06b91df392d76d32be9885008
Chris Ball <ChrisBall@Christophers-MacBook-Air.local>
2018-07-27 10:52:08 +0100

    Survey - v2

--- app/routes/instructions.js
+++ app/routes/instructions.js
@@ -12,4 +12,18 @@ module.exports = function (router) {
             res.send(html)
         })
     })
+
+    router.get('/name-study/v2/instructions', function (req, res) {
+        // pull in the url parameters
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var country = req.param('country')
+        var age = req.param('age')
+        var healthcare = req.param('healthcare')
+        // re-render the page along with the parameter
+        res.render('name-study/v2/instructions', { healthcare: healthcare, age: age, country: country, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
 }
