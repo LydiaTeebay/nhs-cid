3482cd85212fc8e12ea7e44f2b5a63cc115408a8
Matt Fielding <matt@hippodigital.co.uk>
2018-08-06 12:04:17 +0100

    name study parameter fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,8 +8,14 @@ module.exports = function (router) {
     router.get('/name-study', function (req, res) {
         // pull in the url parameter
         var lsId = req.param('lsId')
-        var lsAccess = req.param('lsAccess');
-        lsAccess = 'NHSACCESSCODETEST';
+
+        // grab LightSpeed ID if there is one
+        if (req.param('lsAccess') ){
+            var lsAccess = req.param('lsAccess')
+        } else {
+            var lsAccess = req.param('ac')
+        }
+
         var lsStudy = req.param('lsStudy')
         res.render('name-study/index', {lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy}, function(err, html) {
             res.send(html)
