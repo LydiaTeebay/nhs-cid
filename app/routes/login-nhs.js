e1fe3e237a1680781f85f26f508af571feb4a5a8
Matt Fielding <matt@hippodigital.co.uk>
2018-08-21 15:17:03 +0100

    establish dev mode parameter

--- app/routes/login-nhs.js
+++ app/routes/login-nhs.js
@@ -23,6 +23,30 @@ module.exports = function (router) {
     }
   })
 
+    router.get('/create-account/v11/login-nhs', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var newUser = req.param('newUser')
+        var serviceName = req.param('serviceName')
+        var passwordChange = req.param('passwordChange')
+        var terms = req.param('terms')
+        var emailAddress = req.param('emailAddress')
+        var mobileNum = req.param('mobileNum')
+        var verified = req.param('verified')
+        var poluser = req.param('poluser')
+        var result = req.param('result')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        // re-render the page along with the parameter
+        res.render('create-account/v11/login-nhs', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser, result: result, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode }, function(err, html) {
+            res.send(html)
+        })
+
+    })
+
     router.get('/create-account/v10/login-nhs', function (req, res) {
         // pull in the url parameters
         var vouched = req.param('vouch')
@@ -58,9 +82,10 @@ module.exports = function (router) {
         var mobileNum = req.param('mobileNum')
         var verified = req.param('verified')
         var poluser = req.param('poluser')
+        var devMode = req.param('devMode')
         // re-render the page along with the parameter
 
-        res.render('create-account/pb/login-nhs', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser }, function(err, html) {
+        res.render('create-account/pb/login-nhs', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser, devMode: devMode }, function(err, html) {
             res.send(html)
         })
 
--- app/routes/nhs-app-start.js
+++ app/routes/nhs-app-start.js
@@ -1,4 +1,18 @@
 module.exports = function (router) {
+    router.get('/start/v5/nhs-app-start', function (req, res) {
+        // pull in the url parameters
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        // re-render the page along with the parameter
+        res.render('start/v5/nhs-app-start', { service: service, serviceName: serviceName, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/start/v4/nhs-app-start', function (req, res) {
         // pull in the url parameters
         var service = req.param('service')
@@ -6,8 +20,20 @@ module.exports = function (router) {
         var lsId = req.param('lsId')
         var lsAccess = req.param('lsAccess')
         var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
         // re-render the page along with the parameter
-        res.render('start/v4/nhs-app-start', { service: service, serviceName: serviceName, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+        res.render('start/v4/nhs-app-start', { service: service, serviceName: serviceName, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode }, function(err, html) {
+            res.send(html)
+        })
+    })
+
+    router.get('/start/pb/nhs-app-start', function (req, res) {
+        // pull in the url parameters
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var devMode = req.param('devMode')
+        // re-render the page along with the parameter
+        res.render('start/pb/nhs-app-start', { service: service, serviceName: serviceName, devMode: devMode}, function(err, html) {
             res.send(html)
         })
     })
