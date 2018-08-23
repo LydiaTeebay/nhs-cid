05d251256a9dd02410bc343dfc9302305b69f34b
Matt Fielding <matt@hippodigital.co.uk>
2018-08-23 15:33:40 +0100

    add params to terms pages

--- app/routes/terms-conditions.js
+++ app/routes/terms-conditions.js
@@ -12,9 +12,14 @@ module.exports = function (router) {
         var mobileNum = req.param('mobileNum')
         var verified = req.param('verified')
         var poluser = req.param('poluser')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
         // re-render the page along with the parameter
 
-        res.render('terms-of-use/terms-conditions', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser }, function(err, html) {
+        res.render('terms-of-use/terms-conditions', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, devMode: devMode, returnUrl: returnUrl, poluser: poluser, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
             res.send(html)
         })
 
