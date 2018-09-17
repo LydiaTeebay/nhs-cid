297c0ed05a5324a004911af6abfa9ac85c7df9ea
monsterlimo <mattnixon79@googlemal.com>
2018-09-17 15:27:45 +0100

    added missing routig js files
    probably missing params

---
+++ app/routes/service-access-offline-find-place-confirm.js
@@ -0,0 +1,25 @@
+module.exports = function (router) {
+    router.get('/service-access/v11/service-access-offline-find-place-confirm', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        var idType = req.param('idType')
+        var isMobile = req.useragent.isMobile
+        var challenge = req.param('challenge')
+        var hidehead = req.param('hidehead')
+        var changetomobile = 'true'
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('service-access/v11/service-access-offline-find-place-confirm', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, idType: idType, isMobile: isMobile, hidehead: hidehead, challenge: challenge, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, changetomobile: changetomobile }, function(err, html) {
+            res.send(html)
+        })
+    })
+}
