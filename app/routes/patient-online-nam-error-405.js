96c91939b84e9ce6867c27ff4d1c47a11adf071a
Matt Fielding <matt@hippodigital.co.uk>
2018-09-27 13:24:20 +0100

    POL 405 error added

---
+++ app/routes/patient-online-nam-error-405.js
@@ -0,0 +1,36 @@
+module.exports = function (router) {
+    router.get('/patient-online/v11/patient-online-nam-error-405', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('patient-online/v11/patient-online-nam-error-405', { serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
+    router.get('/patient-online/v10/patient-online-nam-error-405', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('patient-online/v10/patient-online-nam-error-405', { serviceName: serviceName, hidehead: hidehead, service: service, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
+    router.get('/patient-online/pb/patient-online-nam-error-405', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        res.render('patient-online/pb/patient-online-nam-error-405', { serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service }, function(err, html) {
+            res.send(html)
+        })
+    })
+}
