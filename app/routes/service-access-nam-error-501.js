0a42c3c6b135c7ee69d983244ef55a1893763774
Matt Fielding <matt@hippodigital.co.uk>
2018-10-15 15:42:55 +0100

    new login error pages for Nam

---
+++ app/routes/service-access-nam-error-501.js
@@ -0,0 +1,25 @@
+module.exports = function (router) {
+    router.get('/service-access/v12/service-access-nam-error-501', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('service-access/v12/service-access-nam-error-501', { serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
+    router.get('/service-access/pb/service-access-nam-error-501', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        res.render('service-access/pb/service-access-nam-error-501', { serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service }, function(err, html) {
+            res.send(html)
+        })
+    })
+}
