fe9d29bcb2af62e1a5bd33e64174d53e85b1eea7
monsterlimo <mattnixon79@googlemal.com>
2018-10-18 22:27:40 +0100

    passed mobile back through
    mobile number is taken throughout the process - not sure if the code is triggered or not

--- app/routes/service-access-start-no-pol.js
+++ app/routes/service-access-start-no-pol.js
@@ -9,13 +9,14 @@ module.exports = function (router) {
         var hidehead = req.param('hidehead')
         var poluser = req.param('poluser')
         var signedin = req.param('signedin')
+        var mobileNum = req.param('mobileNum')
         var lsId = req.param('lsId')
         var lsAccess = req.param('lsAccess')
         var lsStudy = req.param('lsStudy')
         var devMode = req.param('devMode')
         var returnUrl = req.param('returnUrl')
         // re-render the page along with the parameter
-        res.render('service-access/v12/service-access-start-no-pol', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, hidehead: hidehead, poluser: poluser, signedin: signedin, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+        res.render('service-access/v12/service-access-start-no-pol', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, hidehead: hidehead, poluser: poluser, signedin: signedin, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, mobileNum: mobileNum }, function(err, html) {
             res.send(html)
         })
     })
