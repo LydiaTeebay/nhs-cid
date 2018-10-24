295cd292c88fa66d51bb8a63d57206686cffbb40
Matt Fielding <matt@hippodigital.co.uk>
2018-10-24 15:54:06 +0100

    remove 2nd triage question

--- app/routes/patient-online-details-2.js
+++ app/routes/patient-online-details-2.js
@@ -8,21 +8,6 @@ module.exports = function (router) {
     })
   })
 
-    router.get('/patient-online/v13/patient-online-details-2', function (req, res) {
-        var service = req.param('service')
-        var serviceName = req.param('serviceName')
-        var hidehead = req.param('hidehead')
-        var devMode = req.param('devMode')
-        var returnUrl = req.param('returnUrl')
-        var poluser = req.param('poluser')
-        var lsId = req.param('lsId')
-        var lsAccess = req.param('lsAccess')
-        var lsStudy = req.param('lsStudy')
-        res.render('patient-online/v13/patient-online-details-2', { serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service, poluser: poluser, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
-            res.send(html)
-        })
-    })
-
     router.get('/patient-online/v12/patient-online-details-2', function (req, res) {
         var service = req.param('service')
         var serviceName = req.param('serviceName')
