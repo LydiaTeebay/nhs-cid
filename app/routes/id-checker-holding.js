2162a61c13997eae0c2dd436d396db33bc5aadb1
Matt Fielding <matt@hippodigital.co.uk>
2018-11-08 13:00:53 +0000

    content changes to id checker

---
+++ app/routes/id-checker-holding.js
@@ -0,0 +1,23 @@
+module.exports = function (router) {
+    router.get('/id-checker/id-checker-holding', function (req, res) {
+        // pull in the url parameter
+        var state = req.param('state')
+        var queue = req.param('queue')
+        var holding = req.param('holding')
+        var role = req.param('role')
+        var emailLink = req.param('emailLink')
+        var result = req.param('result')
+        var demographics = req.param('demographics')
+        var devmode = req.param('devMode')
+        var noabort = req.param('noabort')
+
+        if (role === 'Supervisor') {
+            res.redirect('/id-checker/id-checker-supervisor-dashboard?role=Supervisor' + '&emailLink=' + emailLink + '&state=' + state + '&result=' + result + '&demographics=' + demographics + '&queue=' + queue + '&holding=' + holding + '&noabort=' + noabort )
+            return
+        }
+        res.render('id-checker/id-checker-holding', {state: state, emailLink: emailLink, role: role, result: result, demographics: demographics, devMode: devmode, queue: queue, holding: holding, noabort: noabort }, function(err, html) {
+            res.send(html)
+        })
+
+    })
+}
