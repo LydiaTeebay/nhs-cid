635149c752ef059369f1453b299c0332f4df4a49
monsterlimo <mattnixon79@googlemal.com>
2018-11-19 11:24:10 +0000

    added three routes for challenge response
    need to improve how the routing works to grab the challenge param - to do

--- app/routes/service-access-video-selfie-camera.js
+++ app/routes/service-access-video-selfie-camera.js
@@ -10,13 +10,12 @@ module.exports = function (router) {
         var changetomobile = req.param('changetomobile')
         var hidehead = req.param('hidehead')
         // is the user on a mobile device?
-        var isMobile = req.useragent.isMobile
+        var isMobile = req.useragent.isMobile        
         var pinCode1 = Math.floor(0 + (9 - 0) * Math.random())
         var pinCode2 = Math.floor(0 + (9 - 0) * Math.random())
         var pinCode3 = Math.floor(0 + (9 - 0) * Math.random())
         var pinCode4 = Math.floor(0 + (9 - 0) * Math.random())
         var challenge = req.param('challenge')
-
         var pinCode = pinCode1 + ' ' + pinCode2 + ' ' + pinCode3 + ' ' + pinCode4
         // re-render the page along with the parameter
         res.render('service-access/service-access-video-selfie-camera', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, changetomobile: changetomobile, mobile: isMobile, pinCode: pinCode, hidehead: hidehead, challenge: challenge }, function(err, html) {
@@ -39,24 +38,29 @@ module.exports = function (router) {
         var isMobileOs = req.useragent.platform
         var isMobileOsVer = parseFloat((req.useragent.source.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/)||[''])[0].replace(/_/g,'.'))
         var isMobileOsV = isMobileOsVer
-        var pinCode1 = Math.floor(0 + (9 - 0) * Math.random())
-        var pinCode2 = Math.floor(0 + (9 - 0) * Math.random())
-        var pinCode3 = Math.floor(0 + (9 - 0) * Math.random())
-        var pinCode4 = Math.floor(0 + (9 - 0) * Math.random())
+        //console.log("testing pin code");
         var challenge = req.param('challenge')
+        if (challenge === "hands") {
+            var pinCode1 = Math.floor(Math.random() * 6)
+            var pinCode2 = Math.floor(Math.random() * 6)
+            var pinCode3 = Math.floor(Math.random() * 6)
+            var pinCode4 = Math.floor(Math.random() * 6)
+        } else {
+            var pinCode1 = Math.floor(0 + (9 - 0) * Math.random())
+            var pinCode2 = Math.floor(0 + (9 - 0) * Math.random())
+            var pinCode3 = Math.floor(0 + (9 - 0) * Math.random())
+            var pinCode4 = Math.floor(0 + (9 - 0) * Math.random())
+        }
+        
         var lsId = req.param('lsId')
         var lsAccess = req.param('lsAccess')
         var lsStudy = req.param('lsStudy')
         var devMode = req.param('devMode')
         var returnUrl = req.param('returnUrl')
 
-        //if (challenge !== 'undefined') {
-        //    var formerror = challenge
-        //}
-
         var pinCode = pinCode1 + ' ' + pinCode2 + ' ' + pinCode3 + ' ' + pinCode4
         // re-render the page along with the parameter
-        res.render('service-access/v13/service-access-video-selfie-camera', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, changetomobile: changetomobile, mobile: isMobile, mobileOs: isMobileOs, mobileOsV: isMobileOsV, pinCode: pinCode, hidehead: hidehead, challenge: challenge, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+        res.render('service-access/v13/service-access-video-selfie-camera', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, changetomobile: changetomobile, mobile: isMobile, mobileOs: isMobileOs, mobileOsV: isMobileOsV, pinCode: pinCode, hidehead: hidehead, challenge: challenge, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl}, function(err, html) {
             res.send(html)
         })
     })
