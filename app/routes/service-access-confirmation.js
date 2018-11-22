fb75cb78c42602dfaa0cc7089312714f638e5e35
Matt Fielding <matt@hippodigital.co.uk>
2018-11-22 13:23:44 +0000

    confirmation bug fix

--- app/routes/service-access-confirmation.js
+++ app/routes/service-access-confirmation.js
@@ -26,9 +26,8 @@ module.exports = function (router) {
         var lsStudy = req.param('lsStudy')
         var devMode = req.param('devMode')
         var returnUrl = req.param('returnUrl')
-        var complete = req.param('complete')
         // re-render the page along with the parameter
-        res.render('service-access/v13/service-access-confirmation', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, hidehead: hidehead, poluser: poluser, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, complete: complete }, function(err, html) {
+        res.render('service-access/v13/service-access-confirmation', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, hidehead: hidehead, poluser: poluser, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
             res.send(html)
         })
     })
