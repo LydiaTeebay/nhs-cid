75b07e7cf318963e88457090c23b87123d693580
Matt Fielding <matt@hippodigital.co.uk>
2018-12-18 09:10:24 +0000

    fix for routing

--- app/routes/service-access-video-selfie-instructions-2.js
+++ app/routes/service-access-video-selfie-instructions-2.js
@@ -26,16 +26,20 @@ module.exports = function (router) {
         var serviceName = req.param('serviceName')
         var mobileNum = req.param('mobileNum')
         var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
         var changetomobile = req.param('changetomobile')
         var hidehead = req.param('hidehead')
+        var challenge = req.param('challenge')
         var pinCode = Math.floor(1000 + Math.random() * 9000)
         // is the user on a mobile device?
         var isMobile = req.useragent.isMobile
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
         var devMode = req.param('devMode')
         var returnUrl = req.param('returnUrl')
-        var genericButton = req.param('genericButton')
         // re-render the page along with the parameter
-        res.render('service-access/v14/service-access-video-selfie-instructions-2', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, changetomobile: changetomobile, mobile: isMobile, pinCode: pinCode, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, genericButton: genericButton }, function(err, html) {
+        res.render('service-access/v14/service-access-video-selfie-instructions-2', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, changetomobile: changetomobile, mobile: isMobile, pinCode: pinCode, hidehead: hidehead, challenge: challenge, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
             res.send(html)
         })
     })
