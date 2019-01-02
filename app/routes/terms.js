c3d377477fa7d54c290c5b024395c30158e5f2e0
Chris Ball <ChrisBall@Christophers-MacBook-Air.local>
2019-01-02 15:09:15 +0000

    Create account JS files

--- app/routes/terms.js
+++ app/routes/terms.js
@@ -17,6 +17,25 @@ module.exports = function (router) {
     })
   })
 
+    router.get('/create-account/v15/terms', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var newUser = req.param('newUser')
+        var serviceName = req.param('serviceName')
+        var passwordChange = req.param('passwordChange')
+        var terms = req.param('terms')
+        var emailAddress = req.param('emailAddress')
+        var mobileNum = req.param('mobileNum')
+        var verified = req.param('verified')
+        var poluser = req.param('poluser')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('create-account/v15/terms', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+            res.send(html)
+        })
+    })
     router.get('/create-account/v14/terms', function (req, res) {
         // pull in the url parameters
         var vouched = req.param('vouch')
