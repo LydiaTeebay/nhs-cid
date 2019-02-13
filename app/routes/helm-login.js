cd97649730b386fc2cc5450180f2ce805277c137
Matt Fielding <matt@hippodigital.co.uk>
2019-02-13 22:52:36 +0000

    start pages

--- app/routes/helm-login.js
+++ app/routes/helm-login.js
@@ -1,4 +1,15 @@
 module.exports = function (router) {
+    router.get('/start/v10/helm-login', function (req, res) {
+        // pull in the url parameters
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('start/v10/helm-login', { service: service, serviceName: serviceName, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+            res.send(html)
+        })
+    })
     router.get('/start/v9/helm-login', function (req, res) {
         // pull in the url parameters
         var service = req.param('service')
@@ -9,7 +20,6 @@ module.exports = function (router) {
         res.render('start/v9/helm-login', { service: service, serviceName: serviceName, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
             res.send(html)
         })
-
     })
     router.get('/start/v8/helm-login', function (req, res) {
         // pull in the url parameters
--- app/routes/helm.js
+++ app/routes/helm.js
@@ -1,4 +1,16 @@
 module.exports = function (router) {
+    router.get('/start/v10/helm', function (req, res) {
+        // pull in the url parameters
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('start/v10/helm', { service: service, serviceName: serviceName, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+            res.send(html)
+        })
+
+    })
     router.get('/start/v9/helm', function (req, res) {
         // pull in the url parameters
         var service = req.param('service')
