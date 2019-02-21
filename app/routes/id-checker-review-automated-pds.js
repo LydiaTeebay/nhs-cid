9c9e48087476a05d8fc82873af93290b86499d13
Matt Fielding <matt@hippodigital.co.uk>
2019-02-21 13:50:16 +0000

    added routing to new id checker screens

---
+++ app/routes/id-checker-review-automated-pds.js
@@ -0,0 +1,15 @@
+module.exports = function (router) {
+    router.get('/id-checker/id-checker-review-automated-pds', function (req, res) {
+        // pull in the url parameter
+        var state = req.param('state')
+        var role = req.param('role')
+        var result = req.param('result')
+        var demographics = req.param('demographics')
+        var failreason = req.param('failreason')
+        var formerror = req.param('formerror')
+        var devMode = req.param('devMode')
+        res.render('id-checker/id-checker-review-automated-pds', { state: state, role: role, result: result, demographics: demographics, failreason: failreason, formerror: formerror, devMode: devMode }, function(err, html) {
+            res.send(html)
+        })
+    })
+}
