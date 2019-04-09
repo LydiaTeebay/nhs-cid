7a1b27436babbf2fc06ca17941122d67ccbcb401
Matt Fielding <matt@hippodigital.co.uk>
2019-04-09 14:09:07 +0100

    ID checker updates

--- app/routes/id-checker-supervisor-review.js
+++ app/routes/id-checker-supervisor-review.js
@@ -2,11 +2,17 @@ module.exports = function (router) {
     router.get('/id-checker/id-checker-supervisor-review', function (req, res) {
         // pull in the url parameter
         var state = req.param('state')
+        var queue = req.param('queue')
+        var holding = req.param('holding')
         var role = req.param('role')
+        var emailLink = req.param('emailLink')
         var result = req.param('result')
         var demographics = req.param('demographics')
+        var devmode = req.param('devMode')
+        var noabort = req.param('noabort')
         var failreason = req.param('failreason')
-        res.render('id-checker/id-checker-supervisor-review', { state: state, role: role, result: result, demographics: demographics, failreason: failreason }, function(err, html) {
+
+        res.render('id-checker/id-checker-supervisor-review', { state: state, emailLink: emailLink, role: role, result: result, demographics: demographics, devMode: devmode, queue: queue, holding: holding, noabort: noabort, failreason: failreason }, function(err, html) {
             res.send(html)
         })
     })
