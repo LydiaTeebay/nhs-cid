0732bb52fd2d04dc865a4c7ebc05eb438128a40d
Matt Fielding <matt@hippodigital.co.uk>
2019-05-16 17:09:06 +0100

    login error scenarios

---
+++ app/routes/login-nhs-error-04.js
@@ -0,0 +1,55 @@
+module.exports = function (router) {
+  router.get('/create-account/v16/login-nhs-error-04', function (req, res) {
+    // pull in the url parameters
+    var vouched = req.param('vouch')
+    var service = req.param('service')
+    var newUser = req.param('newUser')
+    var serviceName = req.param('serviceName')
+    var passwordChange = req.param('passwordChange')
+    var terms = req.param('terms')
+    var emailAddress = req.param('emailAddress')
+    var mobileNum = req.param('mobileNum')
+    var verified = req.param('verified')
+    var poluser = req.param('poluser')
+    // re-render the page along with the parameter
+    res.render('create-account/v16/login-nhs-error-04', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser }, function(err, html) {
+      res.send(html)
+    })
+  })
+
+  router.get('/create-account/demo/login-nhs-error-04', function (req, res) {
+    // pull in the url parameters
+    var vouched = req.param('vouch')
+    var service = req.param('service')
+    var newUser = req.param('newUser')
+    var serviceName = req.param('serviceName')
+    var passwordChange = req.param('passwordChange')
+    var terms = req.param('terms')
+    var emailAddress = req.param('emailAddress')
+    var mobileNum = req.param('mobileNum')
+    var verified = req.param('verified')
+    var poluser = req.param('poluser')
+    // re-render the page along with the parameter
+    res.render('create-account/demo/login-nhs-error-04', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser }, function(err, html) {
+      res.send(html)
+    })
+  })
+
+  router.get('/create-account/pb/login-nhs-error-04', function (req, res) {
+    // pull in the url parameters
+    var vouched = req.param('vouch')
+    var service = req.param('service')
+    var newUser = req.param('newUser')
+    var serviceName = req.param('serviceName')
+    var passwordChange = req.param('passwordChange')
+    var terms = req.param('terms')
+    var emailAddress = req.param('emailAddress')
+    var mobileNum = req.param('mobileNum')
+    var verified = req.param('verified')
+    var poluser = req.param('poluser')
+    // re-render the page along with the parameter
+    res.render('create-account/pb/login-nhs-error-04', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, poluser: poluser }, function(err, html) {
+      res.send(html)
+    })
+  })
+}
