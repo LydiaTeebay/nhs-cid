d6314d30ac6210f8b6044b29d4d7f64a5ce0bcce
Matt Fielding <matt@hippodigital.co.uk>
2019-07-02 16:45:03 +0100

    Start v11

--- app/routes/nhs-app-start.js
+++ app/routes/nhs-app-start.js
@@ -1,4 +1,18 @@
 module.exports = function (router) {
+    router.get('/start/v11/nhs-app-start', function (req, res) {
+        // pull in the url parameters
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('start/v11/nhs-app-start', { service: service, serviceName: serviceName, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+            res.send(html)
+        })
+    })
     router.get('/start/v10/nhs-app-start', function (req, res) {
         // pull in the url parameters
         var service = req.param('service')
