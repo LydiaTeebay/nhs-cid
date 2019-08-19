9649fb22115fa2aa22cf50a70055c4c5647d1dd3
Matt Fielding <matt@hippodigital.co.uk>
2019-08-19 11:46:03 +0100

    v18 of Service Access

--- app/routes/service-access-nhs-number-demographics-postcode.js
+++ app/routes/service-access-nhs-number-demographics-postcode.js
@@ -14,6 +14,27 @@ module.exports = function (router) {
         })
     })
 
+    router.get('/service-access/v18/service-access-nhs-number-demographics-postcode', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        var hidehead = req.param('hidehead')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var idType = req.param('idType')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('service-access/v18/service-access-nhs-number-demographics-postcode', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, hidehead: hidehead, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, idType: idType }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/service-access/v17/service-access-nhs-number-demographics-postcode', function (req, res) {
         // pull in the url parameters
         var vouched = req.param('vouch')
--- app/routes/service-access-photo-id-camera.js
+++ app/routes/service-access-photo-id-camera.js
@@ -26,6 +26,35 @@ module.exports = function (router) {
         })
     })
 
+    router.get('/service-access/v18/service-access-photo-id-camera', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        var idType = req.param('idType')
+        var changetomobile = req.param('changetomobile')
+        var hidehead = req.param('hidehead')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // is the user on a mobile device?
+        var isMobile = req.useragent.isMobile
+        var isMobileOs = req.useragent.platform
+        var isMobileOsVer = parseFloat((req.useragent.source.match(/\b[0-9]+_[0-9]+(?:_[0-9]+)?\b/)||[''])[0].replace(/_/g,'.'))
+        var isMobileOsV = isMobileOsVer
+        var upload = req.param('upload')
+
+        // re-render the page along with the parameter
+        res.render('service-access/v18/service-access-photo-id-camera', { vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, idType: idType, changetomobile: changetomobile, mobile: isMobile, mobileOs: isMobileOs, mobileOsV: isMobileOsV, hidehead: hidehead, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, upload: upload }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/service-access/v17/service-access-photo-id-camera', function (req, res) {
         // pull in the url parameters
         var vouched = req.param('vouch')
