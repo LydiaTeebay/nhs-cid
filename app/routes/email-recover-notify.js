1c63a0fd39b70b3f996547e0f11554690072b48d
Will <ws.hepworth@gmail.com>
2019-09-18 17:14:39 +0100

    Routing for account recovery dev mode

---
+++ app/routes/email-recover-notify.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+module.exports = function (router) {
+  router.get('/emails/email-recover-notify/', function (req, res) {
+    // pull in the url parameters
+    var theReason = req.param('reason')
+    var vouched = req.param('vouch')
+    var service = req.param('service')
+    var serviceName = req.param('serviceName')
+    var terms = req.param('terms')
+    var resend = req.param('resend')
+    var mobileNum = req.param('mobileNum')
+    var emailAddress = req.param('emailAddress')
+    var verified = req.param('verified')
+    var recoveryEmail = req.param('recoveryEmail')
+    var pinCode = Math.floor(1000 + Math.random() * 9000)
+    var personalisation = {
+        'pincode': pinCode
+    }
+    var poluser = req.param('poluser')
+
+    // re-render the page along with the parameter
+    res.render('/emails/email-recover-notify/', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName, terms: terms, resend: resend, mobileNum: mobileNum, emailAddress: emailAddress, verified: verified, poluser: poluser, recoveryEmail: recoveryEmail }, function(err, html) {
+      res.send(html)
+    })
+  })
+}
---
+++ app/routes/recovery-email.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+module.exports = function (router) {
+    router.get('/create-account/v18/recovery-email', function (req, res) {
+        // pull in the url parameters
+        var theReason = req.param('reason')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var terms = req.param('terms')
+        var resend = req.param('resend')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var verified = req.param('verified')
+        var poluser = req.param('poluser')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var formerror = req.param('formerror')
+        var pinCode = Math.floor(100000 + Math.random() * 900000)
+
+        var personalisation = {
+            'pincode': pinCode
+        }
+        var reason = req.param('reason')
+        var result = req.param('result')
+        
+        // re-render the page along with the parameter
+        res.render('create-account/v18/recovery-email', {reason: theReason, vouch: vouched, service: service, serviceName: serviceName, devMode: devMode, returnUrl: returnUrl, terms: terms, resend: resend, mobileNum: mobileNum, emailAddress: emailAddress, verified: verified, poluser: poluser, result: result, reason: reason, formerror: formerror }, function(err, html) {
+            res.send(html)
+        })
+    })
+
+
+}
