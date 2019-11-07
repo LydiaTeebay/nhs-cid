5151e0f40afc2331b1d583354b0536ed6e854af0
Matt Fielding <matt@hippodigital.co.uk>
2019-11-07 17:30:17 +0000

    new PYI routing

--- app/routes/service-access-what-you-need.js
+++ app/routes/service-access-what-you-need.js
@@ -17,8 +17,9 @@ module.exports = function (router) {
         var returnUrl = req.param('returnUrl')
         var iproov = req.param('iproov')
         var desk = req.param('desk')
+        var drop = req.param('drop')
         // re-render the page along with the parameter
-        res.render('service-access/v19/service-access-what-you-need', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, hidehead: hidehead, poluser: poluser, loggedin: loggedin, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, iproov: iproov, desk: desk }, function(err, html) {
+        res.render('service-access/v19/service-access-what-you-need', {vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, hidehead: hidehead, poluser: poluser, loggedin: loggedin, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, iproov: iproov, desk: desk, drop: drop }, function(err, html) {
             res.send(html)
         })
     })
