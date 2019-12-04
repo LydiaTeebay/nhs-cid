26363a6efd323ccaaf15276d1ecb8fef47ae0c06
Will <ws.hepworth@gmail.com>
2019-12-04 14:21:04 +0000

    Updated titles and tweaked H1

--- app/routes/terms-conditions.js
+++ app/routes/terms-conditions.js
@@ -25,4 +25,29 @@ module.exports = function (router) {
 
     })
 
+    router.get('/terms-of-use/v2/terms-conditions', function (req, res) {
+        // pull in the url parameters
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var newUser = req.param('newUser')
+        var serviceName = req.param('serviceName')
+        var passwordChange = req.param('passwordChange')
+        var terms = req.param('terms')
+        var emailAddress = req.param('emailAddress')
+        var mobileNum = req.param('mobileNum')
+        var verified = req.param('verified')
+        var poluser = req.param('poluser')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        // re-render the page along with the parameter
+
+        res.render('terms-of-use/v2/terms-conditions', { emailAddress: emailAddress, mobileNum: mobileNum, vouch: vouched, passwordChange: passwordChange, newUser: newUser, service: service, serviceName: serviceName, terms: terms, verified: verified, devMode: devMode, returnUrl: returnUrl, poluser: poluser, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+
+    })
+
 }
