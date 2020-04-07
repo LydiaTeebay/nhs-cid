51fde7c5379f70e07f96252c28dce02111af535d
Matt Fielding <matt@hippodigital.co.uk>
2020-04-07 12:27:38 +0100

    v21 of patient online

--- app/routes/patient-online-linkage-key.js
+++ app/routes/patient-online-linkage-key.js
@@ -12,6 +12,30 @@ module.exports = function (router) {
     })
   })
 
+    router.get('/patient-online/v21/patient-online-linkage-key', function (req, res) {
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var hidehead = req.param('hidehead')
+        var poluser = req.param('poluser')
+        var system = req.param('system')
+        var practiceID = req.param('practiceID')
+        var loggedin = req.param('loggedin')
+        var result = req.param('result')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var pyiuser = req.param('pyiuser')
+        var manual = req.param('manual')
+        res.render('patient-online/v21/patient-online-linkage-key', { vouch: vouched, service: service, serviceName: serviceName, mobileNum: mobileNum, emailAddress: emailAddress, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, poluser: poluser, system: system, practiceID: practiceID, loggedin: loggedin, result: result, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, pyiuser : pyiuser, manual: manual }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/patient-online/v20/patient-online-linkage-key', function (req, res) {
         var vouched = req.param('vouch')
         var service = req.param('service')
--- app/routes/patient-online-nam-error-400.js
+++ app/routes/patient-online-nam-error-400.js
@@ -1,4 +1,18 @@
 module.exports = function (router) {
+    router.get('/patient-online/v21/patient-online-nam-error-400', function (req, res) {
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('patient-online/v21/patient-online-nam-error-400', { serviceName: serviceName, hidehead: hidehead, service: service, devMode: devMode, returnUrl: returnUrl, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/patient-online/v20/patient-online-nam-error-400', function (req, res) {
         var service = req.param('service')
         var serviceName = req.param('serviceName')
