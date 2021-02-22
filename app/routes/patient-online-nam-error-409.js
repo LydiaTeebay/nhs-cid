cdac9fd051617c56b7f49a1ae305dc54a146494b
dave <david.scattergood@hippodigital.co.uk>
2021-02-22 12:38:19 +0000

    POL js files updated to v23, added target _blank links

--- app/routes/patient-online-nam-error-409.js
+++ app/routes/patient-online-nam-error-409.js
@@ -1,4 +1,20 @@
 module.exports = function (router) {
+
+    router.get('/patient-online/v23/patient-online-nam-error-409', function (req, res) {
+        var uplift = req.param('uplift')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var hidehead = req.param('hidehead')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        res.render('patient-online/v23/patient-online-nam-error-409', { uplift: uplift, serviceName: serviceName, hidehead: hidehead, devMode: devMode, returnUrl: returnUrl, service: service, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy }, function(err, html) {
+            res.send(html)
+        })
+    })
+
     router.get('/patient-online/v22/patient-online-nam-error-409', function (req, res) {
         var uplift = req.param('uplift')
         var service = req.param('service')
