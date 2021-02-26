122e368c96c168abf1bf9351a7e52391669c3fe5
dave <david.scattergood@hippodigital.co.uk>
2021-02-26 10:59:43 +0000

    Service-access-steps Created 4 new files V1 for separate steps 1-4

---
+++ app/routes/service-access-steps_v1_step3-of-4.js
@@ -0,0 +1,24 @@
+module.exports = function (router) {
+    router.get('/service-access/v23/service-access-steps_v1_step3-of-4', function (req, res) {
+        // pull in the url parameters
+        var uplift = req.param('uplift')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var mobileNum = req.param('mobileNum')
+        var emailAddress = req.param('emailAddress')
+        var formerror = req.param('formerror')
+        var hidehead = req.param('hidehead')
+        var poluser = req.param('poluser')
+        var loggedin = req.param('loggedin')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        // re-render the page along with the parameter
+        res.render('service-access/v23/service-access-steps_v1_step3-of-4', { uplift: uplift, vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, formerror: formerror, hidehead: hidehead, poluser: poluser, loggedin: loggedin, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl }, function(err, html) {
+            res.send(html)
+        })
+    })   
+}
