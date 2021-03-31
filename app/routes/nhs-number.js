551ffb9636fc90467578bb185ae34b9de2bf5875
Em779 <57357358+Em779@users.noreply.github.com>
2021-03-31 14:30:53 +0100

    nhs number radio

--- app/routes/nhs-number.js
+++ app/routes/nhs-number.js
@@ -13,6 +13,37 @@ module.exports = function (router) {
             res.send(html)
         })
     })
+    
+    // service-access-nhs-number-automated
+    router.post('/service-access/v23/service-access-nhs-number-automated', function (req, res) {
+        // pull in the url parameters
+        var uplift = req.param('uplift')
+        var emailAddress = req.param('emailAddress')
+        var mobileNum = req.param('mobileNum')
+        var idType = req.body.idType
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var isMobile = req.useragent.isMobile
+        var formerror = req.param('formerror')
+        var hidehead = req.param('hidehead')
+        var challenge = req.param('challenge')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess')
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var iproov = req.param('iproov')
+        var desk = req.param('desk')
+        var manual = req.param('manual')
+        var side = 'front'
+        if (idType === 'yes' ) {
+            res.redirect('/service-access/v23/service-access-nhs-number-demographics-dob=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual  + '&side=' + side + '&uplift=' + uplift)
+        }
+
+        else {
+            res.redirect('/service-access/v23/service-access-nhs-number-demographics-name' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual + '&side=' + side + '&uplift=' + uplift)
+        }
+    })
 
     router.post('/service-access/v2/nhs-number', function (req, res) {
         console.log("post")
@@ -26,6 +57,7 @@ module.exports = function (router) {
         if (nhs_yes === 'yes') {
             check('nhs_number').exists()
             check('nhs_number', 'NHS numbers must be at least 10 digits and can contain spaces').isLength({min: 10}).isNumeric()
+
         }
 
         res.redirect('/service-access/v2/service-access-eredbook-03?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + "&serviceName=" + serviceName)
