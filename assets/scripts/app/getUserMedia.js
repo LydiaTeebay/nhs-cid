2f1bebfb3d2e3bb44a3275d2c33cb3ba6c1227cb
Em779 <57357358+Em779@users.noreply.github.com>
2021-05-26 17:27:53 +0100

    html5

--- assets/scripts/app/getUserMedia.js
+++ assets/scripts/app/getUserMedia.js
@@ -27,7 +27,7 @@ var playButton = document.querySelector('button#play');
 var downloadButton = document.querySelector('button#download');
 var submitButton = document.querySelector('button#submitvideo');
 var retakeButton = document.querySelector('a#retakevideo');
-recordButton.onclick = toggleRecording;
+recordButton.onclick = startStop;
 playButton.onclick = play;
 downloadButton.onclick = download;
 submitButton.onclick = showUploader;
@@ -135,6 +135,9 @@ function startRecording() {
     console.log('MediaRecorder started', mediaRecorder);
 }
 
+
+
+
 function stopRecording() {
     mediaRecorder.stop();
     recordButton.className = "button";
@@ -223,4 +226,112 @@ function download() {
 function trace(arg) {
     var now = (window.performance.now() / 1000).toFixed(3);
     console.log(now + ': ', arg);
+}
+
+
+
+
+//Stopwatch JS 
+
+//Define vars to hold time values
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+//Define vars to hold "display" value
+let displaySeconds = 0;
+let displayMinutes = 0;
+let displayHours = 0;
+
+//Define var to hold setInterval() function
+let interval = null;
+
+//Define var to hold stopwatch status
+let status = "stopped";
+
+//Stopwatch function (logic to determine when to increment next value, etc.)
+function stopWatch(){
+
+    seconds++;
+
+    //Logic to determine when to increment next value
+    if(seconds / 60 === 1){
+        seconds = 0;
+        minutes++;
+
+        if(minutes / 60 === 1){
+            minutes = 0;
+            hours++;
+        }
+
+    }
+
+    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
+    if(seconds < 10){
+        displaySeconds = "0" + seconds.toString();
+    }
+    else{
+        displaySeconds = seconds;
+    }
+
+    if(minutes < 10){
+        displayMinutes = "0" + minutes.toString();
+    }
+    else{
+        displayMinutes = minutes;
+    }
+
+    if(hours < 10){
+        displayHours = "0" + hours.toString();
+    }
+    else{
+        displayHours = hours;
+    }
+
+    //Display updated time values to user
+    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+
+}
+
+
+
+function startStop(){
+
+const dot = document.getElementById ("dot")
+
+    if(status === "stopped"){
+
+        //Start the stopwatch (by calling the setInterval() function)
+        interval = window.setInterval(stopWatch, 1000);
+        document.getElementById("startStop").innerHTML = "Stop";
+        status = "started";
+        toggleRecording()
+
+        dot.style.visibility="hidden"
+
+
+    }
+    else{
+
+        window.clearInterval(interval);
+        document.getElementById("startStop").innerHTML = "Start";
+        status = "stopped";
+        toggleRecording()
+        
+        dot.style.visibility="visible"
+
+    }
+
+}
+
+//Function to reset the stopwatch
+function reset(){
+
+    window.clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    document.getElementById("display").innerHTML = "00:00:00";
+    document.getElementById("startStop").innerHTML = "Start";
+
 }
