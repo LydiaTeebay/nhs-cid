1de939eced48badaeb201488f97fdb2a89482bd0
Lydia Teebay <lydia@hippodigital.co.uk>
2021-07-20 10:10:13 +0100

    fixing nhs number automated linking

--- app/routes/service-access-nhs-number-automated.js
+++ app/routes/service-access-nhs-number-automated.js
@@ -30,6 +30,41 @@ module.exports = function (router) {
     }) 
 
 
+
+    router.post('/service-access/v24/service-access-nhs-number-automated', function (req, res) {
+        // pull in the url parameters
+        var uplift = req.param('uplift')
+        var emailAddress = req.param('emailAddress')
+        var nhsNumber = req.body.nhsNumber
+        var mobileNum = req.param('mobileNum')
+        var vouched = req.param('vouch')
+        var service = req.param('service')
+        var serviceName = req.param('serviceName')
+        var isMobile = req.useragent.isMobile
+        var formerror = req.param('formerror')
+        var hidehead = req.param('hidehead')
+        var challenge = req.param('challenge')
+        var lsId = req.param('lsId')
+        var lsAccess = req.param('lsAccess') 
+        var lsStudy = req.param('lsStudy')
+        var devMode = req.param('devMode')
+        var returnUrl = req.param('returnUrl')
+        var iproov = req.param('iproov')
+        var desk = req.param('desk')
+        var manual = req.param('manual')
+        var side = 'front'
+        if (nhsNumber === 'yes' ) {
+            res.redirect('service-access-nhs-number-demographics-dob?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual  + '&side=' + side + '&uplift=' + uplift + '&nhsNumber=' + nhsNumber)
+        } 
+        
+        else {
+            res.redirect('service-access-nhs-number-demographics-name?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual + '&side=' + side + '&uplift=' + uplift + '&nhsNumber=' + nhsNumber)
+        }
+    })
+
+
+
+
     router.get('/service-access/v23/service-access-nhs-number-automated', function (req, res) {
         // pull in the url parameters
         var uplift = req.param('uplift')
