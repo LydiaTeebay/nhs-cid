b32e63df26d7e9f3ee73a6b3678676bfa4b46a80
Lydia Teebay <lydia@hippodigital.co.uk>
2022-01-20 15:32:09 +0000

    PYI routing inital questions

--- app/routes/service-access-routes-question-1.js
+++ app/routes/service-access-routes-question-1.js
@@ -1,48 +1,6 @@
 module.exports = function (router) {
 
-    router.get('/service-access/v24/service-access-routes-question', function (req, res) {
-        // pull in the url parameters
-        var uplift = req.param('uplift')
-        var vouched = req.param('vouch')
-        var service = req.param('service')
-        var serviceName = req.param('serviceName')
-        var mobileNum = req.param('mobileNum')
-        var emailAddress = req.param('emailAddress')
-        var formerror = req.param('formerror')
-        var idType = req.param('idType')
-        var isMobile = req.useragent.isMobile
-        var challenge = req.param('challenge')
-        var hidehead = req.param('hidehead')
-        var changetomobile = 'true'
-        var lsId = req.param('lsId')
-        var lsAccess = req.param('lsAccess')
-        var lsStudy = req.param('lsStudy')
-        var devMode = req.param('devMode')
-        var returnUrl = req.param('returnUrl')
-        var iproov = req.param('iproov')
-        var desk = req.param('desk')
-        var linkback = req.param('linkback')
-        var manual = req.param('manual')
-        var testing = req.param('testing')
-
-        // re-render the page along with the parameter
-
-        if (iproov === 'false') {
-            res.redirect('/service-access/v24/service-access-switchtomobile?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&iproov=' + iproov + '&desk=' + desk + '&linkback=' + linkback + '&manual=' + manual + '&uplift=' + uplift + '&testing=' + testing )
-        }
-
-        else if (desk === 'true') {
-            res.redirect('/service-access/v24/service-access-switchtomobile-desktop?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&iproov=' + iproov + '&desk=' + desk + '&linkback=' + linkback + '&manual=' + manual + '&uplift=' + uplift + '&testing=' + testing )
-        }
-
-        else {
-            res.render('service-access/v24/service-access-routes-question', { testing: testing, uplift: uplift, vouch: vouched, service: service, serviceName: serviceName, emailAddress: emailAddress, mobileNum: mobileNum, idType: idType, hidehead: hidehead, challenge: challenge, lsId: lsId, lsAccess: lsAccess, lsStudy: lsStudy, devMode: devMode, returnUrl: returnUrl, isMobile: isMobile, changetomobile: changetomobile, iproov: iproov, desk: desk, linkback: linkback, manual: manual }, function(err, html) {
-                res.send(html)
-            })
-        }
-    })
-
-    router.post('/service-access/v24/service-access-routes-question', function (req, res) {
+    router.post('/service-access/v24/service-access-routes-question-1', function (req, res) {
         // pull in the url parameters
         var uplift = req.param('uplift')
         var emailAddress = req.param('emailAddress')
@@ -59,16 +17,18 @@ module.exports = function (router) {
         var lsStudy = req.param('lsStudy')
         var devMode = req.param('devMode')
         var returnUrl = req.param('returnUrl')
-        var iproov = req.param('iproov')
-        var desk = req.param('desk')
         var manual = req.param('manual')
+        var testing = req.param('testing')
         var side = 'front'
-        if (idType === 'passport' || idType === 'UK driving licence' || idType === 'European driving licence' || idType === 'European national identity card' ) {
-            res.redirect('/service-access/v24/service-access-photo-id-instructions?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual  + '&side=' + side + '&uplift=' + uplift + '&testing=' + testing )
+        var id = req.body.id
+        if (id === 'Yes' ) {
+            res.redirect('/service-access/v24/service-access-steps_1?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual  + '&side=' + side + '&uplift=' + uplift + '&testing=' + testing )
         }
-
+        else if (id === 'Yes-ish') {
+            res.redirect('/service-access/v24/service-access-device-transition-comeback?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual  + '&side=' + side + '&uplift=' + uplift + '&testing=' + testing )
+        }
         else {
-            res.redirect('/service-access/v24/service-access-offline-triage?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual + '&side=' + side + '&uplift=' + uplift + '&testing=' + testing )
+            res.redirect('/patient-online/v24/patient-online-details?emailAddress=' + emailAddress + '&mobileNum=' + mobileNum + '&service=' + service + '&serviceName=' + serviceName + '&idType=' + idType + '&hidehead=' + hidehead + '&challenge=' + challenge + '&lsId=' + lsId + '&lsAccess=' + lsAccess + '&lsStudy=' + lsStudy + '&devMode=' + devMode + '&returnUrl=' + returnUrl + '&formerror=' + formerror + '&isMobile=' + isMobile + '&manual=' + manual + '&side=' + side + '&uplift=' + uplift + '&testing=' + testing )
         }
     })
     
